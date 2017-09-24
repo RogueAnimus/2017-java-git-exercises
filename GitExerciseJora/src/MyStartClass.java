81be5fb77fb60825c41971a342694c25bb0c147d
Singh Jorawar <singh.jorawar@outlook.com>
2017-09-24 16:10:03 +0200

    Comitted the third file to the repository

---
+++ GitExerciseJora/src/MyStartClass.java
@@ -0,0 +1,14 @@
+public class MyStartClass {
+
+
+
+
+    /* Nothing really to run at the moment.. this is just an exercise around git. */
+    public static void main (String[] args){
+
+
+
+
+    }
+
+}
