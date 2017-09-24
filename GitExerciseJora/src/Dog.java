bf668139fe798dacc65353961770488928c70c47
Singh Jorawar <singh.jorawar@outlook.com>
2017-09-24 16:00:00 +0200

    Comitting the Second class, Dog Subclass, through IntelliJ

---
+++ GitExerciseJora/src/Dog.java
@@ -0,0 +1,76 @@
+public class Dog extends Animal {
+
+
+
+    public String petOwnerID;
+
+
+
+
+
+
+    /**
+     * Constructor
+     */
+    public Dog(){
+        super();
+    }
+
+    /***************************************************/
+    /***     LOADING AND VALIDATING                  ***/
+    /***************************************************/
+
+    /**
+     * Dumb loading method
+     * @return
+     */
+    @Override
+    public boolean loadAttributes() {
+        setName("Dog");
+        setDiet("Pedigree");
+        setAverageAge(10);
+        setContinent("Any");
+        return true;
+    }
+
+    /**
+     * Dumb validation method
+     * @return
+     */
+    @Override
+    public boolean validateAttributes() {
+        boolean output=false;
+        if (!getName().equals("") && !getDiet().equals("") && getAverageAge() != 0 && !getContinent().equals("")){
+            output=true;
+        }
+        return output;
+    }
+
+
+    /***************************************************/
+    /***     GETTERS AND SETTERS                     ***/
+    /***************************************************/
+
+    public String getPetOwnerID() {
+        return petOwnerID;
+    }
+
+    public void setPetOwnerID(String petOwnerID) {
+        this.petOwnerID = petOwnerID;
+    }
+
+
+    /***************************************************/
+    /***     FUNCTIONALITY                           ***/
+    /***************************************************/
+
+
+    /**
+     * Dumb functionality
+     */
+    @Override
+    public void eatFood() {
+        // Go to food given by owner
+        // Eat
+    }
+}
